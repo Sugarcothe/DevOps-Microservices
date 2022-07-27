07a4e5203a5814651362ee8e1443898dcffb4b9b
Sugarcothe <ifeanyivalentine82@gmail.com>
2022-07-27 14:46:30 +0100

    kubernetes and docker deploy

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -5,14 +5,18 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+export dockerpath=val12012/ml-housing-flask:v1.0.0
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+kubectl create deploy ml-housing-flask --image=$dockerpath
 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward pod/ml-housing-flask-84b9d86cf8-292gu --address 0.0.0.0 8000:80
 
--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -6,10 +6,13 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
-
+export dockerpath=val12012/ml-housing-flask:v1.0.0
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+sudo docker tag ml-housing-flask $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+sudo docker login
+sudo docker push $dockerpath
